722c4667fc12f726b9e8b6d1baf42b1c47ab9e06
KTCTC <ktcorporatetrainingcentre@gmail.com>
2022-03-11 22:08:00 +0530

    updated code on 11 March

---
+++ src/object/classs/ArithMaticCalculations.java
@@ -0,0 +1,21 @@
+package object.classs;
+
+public class ArithMaticCalculations {
+	
+	int a = 90;
+	int b = 89;
+	
+	public static void addition()
+	{
+		int c = 34+67;
+		System.out.println(c);
+		
+	}
+	
+	public static void substraction()
+	{
+		int c = 55-44;
+		System.out.println(c);
+	}
+
+}
