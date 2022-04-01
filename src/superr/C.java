fc4213b42a00d9f5d76d46c89dca66d36ae84a6f
KTCTC <ktcorporatetrainingcentre@gmail.com>
2022-04-01 21:46:14 +0530

    updated code 1 April

---
+++ src/superr/C.java
@@ -0,0 +1,20 @@
+package superr;
+
+public class C {
+
+	public C(int a) {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args) {
+		
+		C oo = new C(23);
+		
+	}
+
+	public void sm()
+	{
+		System.out.println("sample method");
+	}
+	
+}
